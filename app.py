85d8a4630509cc2fbb7a6dd578b44cca4cb25c18
agamjolly <agamjolly@berkeley.edu>
2020-05-20 20:04:46 +0530

    added bootstrap ui

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    variable = 10
-    cars = {
-      "Agam": "Maruti 800",
-      "Yash": "Mercedes", 
-      "Boss": "Jisme ladkiyan ho"
-    }
-    return render_template('index.html', cars=cars)
+    return render_template("index.html", title="Home")
+
+@app.route('/login')
+def login():
+    return render_template("login.html", title="Login")
 
 if __name__ == '__main__':
   app.run(host='127.0.0.1', port=8000, debug=True)
